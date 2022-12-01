464bf8d97ceb85931543732e665454fa915630f0
demidko <DanielDemidko@gmail.com>
2022-12-01 10:30:51 +0500

    Initial commit

---
+++ src/main/kotlin/com/adventofcode/Day.kt
@@ -0,0 +1,18 @@
+package com.adventofcode
+
+fun process(line: String) {
+  TODO()
+}
+
+fun solution(): Long {
+  TODO()
+}
+
+fun main() {
+  ::main
+    .javaClass
+    .getResourceAsStream("/input")!!
+    .bufferedReader()
+    .forEachLine(::process)
+  println(solution())
+}
